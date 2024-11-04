b753737655307a6ecd0eb8f87e0ef48773f22dd4
Rodrigo Naray <rodrigo.naray@fing.edu.uy>
2024-11-03 21:06:57 -0300

    Translate all comments to English

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 import fs from 'fs'
 import chalk from 'chalk';
 
-
-// Funcion encargada de cargar las tareas
+// Function responsible for loading tasks
 function loadTasks() {
   try {
-
-    //Verifico si existe el archivo
+    // Check if the file exists
     if (!fs.existsSync(tasksFile)) {
       const taskArray = { nextId: 1, taskArray: [] }
-      // Si no existe procedo a crearlo
-      fs.writeFileSync(tasksFile, JSON.stringify(taskArray)); //Crea el archivo con un array vacio
+      // If it doesn't exist, proceed to create it
+      fs.writeFileSync(tasksFile, JSON.stringify(taskArray)); // Create the file with an empty array
     }
 
-    //Leer el archivo y devolver las tareas
+    // Read the file and return the tasks
     const dataBuffer = fs.readFileSync(tasksFile);
     const dataJSON = dataBuffer.toString()
     return JSON.parse(dataJSON)
@@ -22,11 +20,11 @@ function loadTasks() {
   }
 }
 
-// Función para guardar tareas en el archivo JSON
+// Function to save tasks in the JSON file
 function saveTasks(tasks) {
   try {
-    const dataJSON = JSON.stringify(tasks, null, 2);  // Convierte el objeto a JSON
-    fs.writeFileSync(tasksFile, dataJSON);  // Escribe en el archivo
+    const dataJSON = JSON.stringify(tasks, null, 2);  // Convert the object to JSON
+    fs.writeFileSync(tasksFile, dataJSON);  // Write to the file
     currentTasks = loadTasks()
 
   } catch (error) {
@@ -34,21 +32,20 @@ function saveTasks(tasks) {
   }
 }
 
-//Funcion para agregar una tarea
+// Function to add a task
 function addTask(taskDescription) {
   try {
-
-    const tasks = loadTasks(); // Cargar tareas existentes
-    const actualDate = new Date(); // Obtener la fecha actual
+    const tasks = loadTasks(); // Load existing tasks
+    const actualDate = new Date(); // Get the current date
     const newTask = {
-      'id': tasks.nextId++, // tasks.nextId++ toma el valor actual para el 'Id' y a su vez actualiza el valor de la propiedad nextId de la propiedad "nextId" del archivo JSON
+      'id': tasks.nextId++, // tasks.nextId++ takes the current value for 'Id' and also updates the value of the "nextId" property in the JSON file
       'task': taskDescription,
       'status': 'todo',
       'createdAt': actualDate.toUTCString(),
       'updatedAt': actualDate.toUTCString()
     }
-    tasks.taskArray.push(newTask); // Agregar la nueva tarea
-    saveTasks(tasks) // Guardar tareas actualizadas
+    tasks.taskArray.push(newTask); // Add the new task
+    saveTasks(tasks) // Save updated tasks
     console.log(chalk.green(`Task Id ${newTask.id} - Description: ${newTask.task} added successfully`));
 
   } catch (error) {
@@ -56,16 +53,15 @@ function addTask(taskDescription) {
   }
 }
 
-//Funcion para listar las tareas
+// Function to list tasks
 function listTasks(args) {
   try {
-
     if (args.length === 0) {
       if (currentTasks.taskArray.length > 0) {
         console.log(chalk.blue(`List of tasks: `))
         currentTasks.taskArray.forEach(task => console.log(`Task id ${chalk.magenta(task.id)} - task: ${chalk.cyan(task.task)} - status: ${chalk.yellow(task.status)}`))
       } else {
-        console.log(chalk.yellow("No hay tareas registradas"))
+        console.log(chalk.yellow("No tasks registered"))
       }
     } else {
       switch (args.toString()) {
@@ -92,10 +88,9 @@ function listTasks(args) {
   }
 }
 
-//Funcion para filtrar las tareas por status
+// Function to filter tasks by status
 function filtteredList(args) {
   try {
-
     const filtterdedTasks = currentTasks.taskArray.filter(elemnt => elemnt.status === args.toString())
     if (filtterdedTasks.length === 0) {
       console.log(`There is no task with status ${args}`)
@@ -106,10 +101,9 @@ function filtteredList(args) {
   } catch (error) {
     console.log(chalk.red(`Error - Function filtteredList() - System error filtering the tasks: ${error}`))
   }
-
 };
 
-//Funcion para mostrar la ayuda
+// Function to show help
 function help() {
   console.log(chalk.greenBright('List of available commands: \n'));
   console.log(chalk.gray('# Adding a new task'));
@@ -139,10 +133,9 @@ function help() {
   console.log(chalk.green('Command example:'), chalk.yellow('exit'));
 }
 
-//Funcion para actualizar las tareas
+// Function to update tasks
 function updateTasks(args) {
   try {
-
     let selectedElement = currentTasks.taskArray.find(element => element.id === Number(args[0]))
 
     if (selectedElement !== undefined) {
@@ -155,14 +148,12 @@ function updateTasks(args) {
     }
   } catch (error) {
     console.log(chalk.red(`Error - Function updateTasks() - System error updating the task: ${error}`))
-
   }
 }
 
-//Funcion para eliminar una tarea
+// Function to delete a task
 function deleteTask(args) {
   try {
-
     let selectedElement = currentTasks.taskArray.find(element => element.id === Number(args[0]))
 
     if (selectedElement !== undefined) {
@@ -178,10 +169,10 @@ function deleteTask(args) {
   }
 }
 
-//Funcion para marcar una tarea como en progreso
+// Function to mark a task as in progress
 function markInProgress(args) {
   try {
-
+    // Function to mark a task as in progress
     let selectedElement = currentTasks.taskArray.find(element => element.id === Number(args[0]))
 
     if (selectedElement !== undefined) {
@@ -196,7 +187,7 @@ function markInProgress(args) {
   }
 }
 
-//Funcion para marcar una tarea como completada
+// Function to mark a task as completed
 function markDone(args) {
   try {
     let selectedElement = currentTasks.taskArray.find(element => element.id === Number(args[0]))
@@ -211,7 +202,6 @@ function markDone(args) {
   } catch (error) {
     console.log(chalk.red(`Error - Function markDone() - System error updating the task: ${error}`))
   }
-
 }
 
 function handleCommands(command, args) {
@@ -244,32 +234,28 @@ function handleCommands(command, args) {
       help();
       break;
 
-
     case 'exit':
       console.log(chalk.blue('Thank you for using SuperList, see you next time!'))
       process.exit();
       break;
 
     default:
-      console.log(chalk.red(`Unreconised command:`), chalk.magenta.bold(`${command}.`), chalk.red(`Please select one of the listed commands \n`));
+      console.log(chalk.red(`Unrecognized command:`), chalk.magenta.bold(`${command}.`), chalk.red(`Please select one of the listed commands \n`));
       handleCommands('help');
       break;
   }
 }
 
-
-
-
-// Archivo donde se guardarán las tareas
+// File where tasks will be saved
 const tasksFile = 'tasks.json';
 
-// Variable que almacena las tareas
+// Variable that stores the tasks
 let currentTasks = loadTasks()
 
 console.log(chalk.blue('Welcome to SuperList!'))
 console.log(chalk.greenBright('Type'), chalk.greenBright.bold('"help"'), chalk.greenBright('to see the list of available commands \n'));
 
-// Escuchar la entrada del usuario
+// Listen to user input
 process.stdin.on('data', (data) => {
   const input = data.toString().trim();
   const [command, ...args] = input.split(" ");
